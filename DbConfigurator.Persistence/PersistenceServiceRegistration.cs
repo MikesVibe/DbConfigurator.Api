365cfe372d9a032459af07fddd1a9ec7d466bc22
MikesVibe <miki889-99@wp.pl>
2023-09-29 17:33:44 +0200

    Fix: Add IRegionRepository to services

--- DbConfigurator.Persistence/PersistenceServiceRegistration.cs
+++ DbConfigurator.Persistence/PersistenceServiceRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using DbConfigurator.API.DataAccess;
 using DbConfigurator.API.DataAccess.Repository;
 using DbConfigurator.Application.Contracts.Persistence;
+using DbConfigurator.Persistence.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +24,7 @@ namespace DbConfigurator.Persistence
             services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
 
             services.AddScoped<IDistributionInformationRepository, DistributionInformationRepository>();
+            services.AddScoped<IRegionRepository, RegionRepository>();
 
             return services;
         }

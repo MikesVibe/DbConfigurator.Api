280769c47597660edfc0d14346936f9668d24a90
MikesVibe <miki889-99@wp.pl>
2023-10-13 21:54:35 +0200

    Add: PriorityController

---
+++ DbConfigurator.Api/Controllers/PriorityController.cs
@@ -0,0 +1,64 @@
+﻿using DbConfigurator.Application.Dtos;
+using DbConfigurator.Application.Features.Priority;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbConfigurator.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriorityController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PriorityController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("all", Name = "GetPriority")]
+        public async Task<ActionResult<IEnumerable<PriorityDto>>> GetPriority()
+        {
+            var priority = await _mediator.Send(new GetPriorityListQuery());
+            return Ok(priority);
+        }
+
+        [HttpGet("{priorityId}", Name = "GetPriorityById")]
+        public async Task<ActionResult<PriorityDto>> GetPriorityById(int priorityId)
+        {
+            var priority = await _mediator.Send(new GetPriorityDetailsQuery() { PriorityId = priorityId });
+            return Ok(priority);
+        }
+
+        [HttpPost(Name = "AddPriority")]
+        public async Task<IActionResult> AddPriority([FromBody] PriorityDto priority)
+        {
+            var response = await _mediator.Send(new CreatePriorityCommand() { Priority = priority });
+
+            if (response.IsFailed)
+                return BadRequest();
+
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeletePriority([FromBody] PriorityDto priority)
+        {
+            var response = await _mediator.Send(new DeletePriorityCommand() { Priority = priority });
+
+            if (response.IsFailed)
+                return BadRequest();
+
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdatePriority([FromBody] PriorityDto priority)
+        {
+            var response = await _mediator.Send(new UpdatePriorityCommand() { Priority = priority });
+
+            if (response.IsFailed)
+                return BadRequest();
+
+            return Ok();
+        }
+    }
+}

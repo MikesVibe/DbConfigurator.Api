cf9cc80f6511c84efa99c41f6a8a277dae62785c
MikesVibe <miki889-99@wp.pl>
2023-10-13 21:52:04 +0200

    Add: CountryController

---
+++ DbConfigurator.Api/Controllers/CountryController.cs
@@ -0,0 +1,64 @@
+﻿using DbConfigurator.Application.Dtos;
+using DbConfigurator.Application.Features.Country;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbConfigurator.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CountryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("all", Name = "GetCountry")]
+        public async Task<ActionResult<IEnumerable<CountryDto>>> GetCountry()
+        {
+            var country = await _mediator.Send(new GetCountryListQuery());
+            return Ok(country);
+        }
+
+        [HttpGet("{countryId}", Name = "GetCountryById")]
+        public async Task<ActionResult<CountryDto>> GetCountryById(int countryId)
+        {
+            var country = await _mediator.Send(new GetCountryDetailsQuery() { CountryId = countryId });
+            return Ok(country);
+        }
+
+        [HttpPost(Name = "AddCountry")]
+        public async Task<IActionResult> AddCountry([FromBody] CountryDto country)
+        {
+            var response = await _mediator.Send(new CreateCountryCommand() { Country = country });
+
+            if (response.IsFailed)
+                return BadRequest();
+
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCountry([FromBody] CountryDto country)
+        {
+            var response = await _mediator.Send(new DeleteCountryCommand() { Country = country });
+
+            if (response.IsFailed)
+                return BadRequest();
+
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCountry([FromBody] CountryDto country)
+        {
+            var response = await _mediator.Send(new UpdateCountryCommand() { Country = country });
+
+            if (response.IsFailed)
+                return BadRequest();
+
+            return Ok();
+        }
+    }
+}

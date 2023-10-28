0bde821e25f4eb7858365bb4476f0ba906f2f0e8
MikesVibe <miki889-99@wp.pl>
2023-10-28 12:18:47 +0200

    Update: Activate authorization

--- DbConfigurator.Application/Features/AccountFeature/UserDto.cs
+++ DbConfigurator.Application/Features/AccountFeature/UserDto.cs
@@ -8,7 +8,6 @@ namespace DbConfigurator.Application.Features.AccountFeature
 {
     public class UserDto
     {
-        //public string DisplayName { get; set; }
         public string UserName { get; set; }
         public string Token { get; set; }
     }

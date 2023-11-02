4a56989e2e0266e1b9e8fb3b02ec4eadb0dbc0e8
MikesVibe <miki889-99@wp.pl>
2023-11-02 16:57:56 +0100

    Add: Status controller

---
+++ DbConfigurator.Api/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using DbConfigurator.Application.Contracts.Features.GetDetail;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbConfigurator.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}

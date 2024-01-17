f0139173e446437f2ab98a955ae4a6087bc0f3b1
MikesVibe <miki889-99@wp.pl>
2024-01-17 11:39:55 +0100

    Add: New properties for RegisterDto

--- DbConfigurator.Application/Features/AccountFeature/RegisterDto.cs
+++ DbConfigurator.Application/Features/AccountFeature/RegisterDto.cs
@@ -13,5 +13,9 @@ namespace DbConfigurator.Application.Features.AccountFeature
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
     }
 }
